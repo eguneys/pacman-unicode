a875bed307f7fa796c497cf4d5b30c3ee3733cb7
Emre Guneyler <eguneys@hotmail.com>
2015-03-09 09:46:06 +0200

    pacman caught check added

--- app/scripts/pacman.js
+++ app/scripts/pacman.js
@@ -2,358 +2,391 @@
 'use strict';
 
 function PacmanGame(parent) {
-    this.parent = parent;
-    
-    this.term = null;
+  this.parent = parent;
+  
+  this.term = null;
 
-    this.termConfig = {
-        hud: { x: 3, y: 1 },
-        gameArea: { x: 3, y: 3 }
-    };
+  this.termConfig = {
+    hud: { x: 3, y: 1 },
+    gameArea: { x: 3, y: 3 }
+  };
 
-    this.score = 0;
-    this.highScore = 0;
+  this.gameOver = true;
 
-    this.p1 = null;
-    this.ghosts = null;
-    this.map = null;
+  this.score = 0;
+  this.highScore = 0;
 
-    this.TilePacmanLeft = [new ut.Tile('>', 255, 255, 0), new ut.Tile('-', 255, 255, 0)];
-    this.TilePacmanRight = [new ut.Tile('<', 255, 255, 0), new ut.Tile('-', 255, 255, 0)];
-    this.TilePacmanUp = [new ut.Tile('V', 255, 255, 0), new ut.Tile('|', 255, 255, 0)];
-    this.TilePacmanDown = [new ut.Tile('^', 255, 255, 0), new ut.Tile('|', 255, 255, 0)];
+  this.p1 = null;
+  this.ghosts = null;
+  this.map = null;
+
+  this.TilePacmanLeft = [new ut.Tile('>', 255, 255, 0), new ut.Tile('-', 255, 255, 0)];
+  this.TilePacmanRight = [new ut.Tile('<', 255, 255, 0), new ut.Tile('-', 255, 255, 0)];
+  this.TilePacmanUp = [new ut.Tile('V', 255, 255, 0), new ut.Tile('|', 255, 255, 0)];
+  this.TilePacmanDown = [new ut.Tile('^', 255, 255, 0), new ut.Tile('|', 255, 255, 0)];
 
 
-    this.TilePoint = new ut.Tile('▪', 150, 0, 150);
-    this.TileBigP = new ut.Tile('O', 150, 0, 150);
-    
-    this.TileWall = new ut.Tile(' ', 0, 0, 0, 100, 100, 100);
+  this.TilePoint = new ut.Tile('▪', 150, 0, 150);
+  this.TileBigP = new ut.Tile('O', 150, 0, 150);
+  
+  this.TileWall = new ut.Tile(' ', 0, 0, 0, 100, 100, 100);
 
-    this.TileEmpty = new ut.Tile(' ', 100, 100, 100);
+  this.TileEmpty = new ut.Tile(' ', 100, 100, 100);
 
-    var hudbg = PacmanGame.HudColors.BG;
-    this.TileHudBG = new ut.Tile(' ', 0, 0, 0, hudbg.r, hudbg.g, hudbg.b);
+  var hudbg = PacmanGame.HudColors.BG;
+  this.TileHudBG = new ut.Tile(' ', 0, 0, 0, hudbg.r, hudbg.g, hudbg.b);
 
-    this.onPacmanMove = null;
+  this.onPacmanMove = null;
 }
 
 PacmanGame.HudColors = {
-    BG: { r: 94, g: 39, b: 40 }
+  BG: { r: 94, g: 39, b: 40 }
 };
 
 PacmanGame.GhostColors = {
-    ORANGE: 1,
-    BLUE: 2,
-    GREEN: 3,
-    PURPLE: 4,
-    WHITE: 5
+  ORANGE: 1,
+  BLUE: 2,
+  GREEN: 3,
+  PURPLE: 4,
+  WHITE: 5
 };
 
 PacmanGame.PacmanTiles = {
-    WALL: '#',
-    POINT: '.',
-    BIGP: 'o',
-    'EMPTY': ' '
+  WALL: '#',
+  POINT: '.',
+  BIGP: 'o',
+  'EMPTY': ' '
 };
 
 
 PacmanGame.prototype.generateMap = function() {
-    this.p1 = { x: 14, y: 12,
-                tile: this.TilePacmanLeft[0]
-              };
+  this.p1 = { x: 14, y: 12,
+              tile: this.TilePacmanLeft[0]
+            };
 
-    this.ghosts = [];
-    
-    this.map = [
-        '############################',
-        '#o...........##...........o#',
-        '#.####.#####.##.#####.####.#',
-        '#.####.#####.##.#####.####.#',
-        '#..........................#',
-        '#.####.#.##########.#.####.#',
-        '#......#.....##.....#......#',
-        '######.#####.##.#####.######',
-        '     #.#............#.#     ',
-        '######.#.####  ####.#.######',
-        '.........#        #.........',
-        '######.#.##########.#.######',
-        '     #.#............#.#     ',
-        '######.#.##########.#.######',
-        '#............##............#',
-        '#.####.#####.##.#####.####.#',
-        '#...##................##...#',
-        '###.##.#.##########.#.##.###',
-        '#......#.....##.....#......#',
-        '#.##########.##.##########.#',
-        '#o........................o#',
-        '############################'
-    ];
+  this.ghosts = [];
+  
+  this.map = [
+    '############################',
+    '#o...........##...........o#',
+    '#.####.#####.##.#####.####.#',
+    '#.####.#####.##.#####.####.#',
+    '#..........................#',
+    '#.####.#.##########.#.####.#',
+    '#......#.....##.....#......#',
+    '######.#####.##.#####.######',
+    '     #.#............#.#     ',
+    '######.#.####  ####.#.######',
+    '.........#        #.........',
+    '######.#.##########.#.######',
+    '     #.#............#.#     ',
+    '######.#.##########.#.######',
+    '#............##............#',
+    '#.####.#####.##.#####.####.#',
+    '#...##................##...#',
+    '###.##.#.##########.#.##.###',
+    '#......#.....##.....#......#',
+    '#.##########.##.##########.#',
+    '#o........................o#',
+    '############################'
+  ];
 
-    this.wrapPos = [
-        { x: -1, y: 10,
-          wrapTo: { x: 27, y: 10 }
-        },
-        { x: 28, y: 10,
-          wrapTo: { x: 0, y: 10 }
-        }
-    ];
+  this.wrapPos = [
+    { x: -1, y: 10,
+      wrapTo: { x: 27, y: 10 }
+    },
+    { x: 28, y: 10,
+      wrapTo: { x: 0, y: 10 }
+    }
+  ];
 };
 
 PacmanGame.prototype.getGhostTile = function(color) {
-    var tileChar = 'X';
-    var tile;
-    
-    switch (color) {
-    case PacmanGame.GhostColors.ORANGE:
-        tile = new ut.Tile(tileChar, 150, 75, 0);
-        break;
-    case PacmanGame.GhostColors.BLUE:
-        tile = new ut.Tile(tileChar, 0, 0, 100);
-        break;
-    case PacmanGame.GhostColors.GREEN:
-        tile = new ut.Tile(tileChar, 0, 100, 0);
-        break;
-    case PacmanGame.GhostColors.PURPLE:
-        tile = new ut.Tile(tileChar, 100, 0, 100);
-        break;
-    case PacmanGame.GhostColors.WHITE:
-        tile = new ut.Tile(tileChar, 255, 255, 255);
-        break;
-    }
-    return tile;
+  var tileChar = 'X';
+  var tile;
+  
+  switch (color) {
+  case PacmanGame.GhostColors.ORANGE:
+    tile = new ut.Tile(tileChar, 150, 75, 0);
+    break;
+  case PacmanGame.GhostColors.BLUE:
+    tile = new ut.Tile(tileChar, 0, 0, 100);
+    break;
+  case PacmanGame.GhostColors.GREEN:
+    tile = new ut.Tile(tileChar, 0, 100, 0);
+    break;
+  case PacmanGame.GhostColors.PURPLE:
+    tile = new ut.Tile(tileChar, 100, 0, 100);
+    break;
+  case PacmanGame.GhostColors.WHITE:
+    tile = new ut.Tile(tileChar, 255, 255, 255);
+    break;
+  }
+  return tile;
 };
 
 PacmanGame.prototype.getPacmanTile = function(x, y) {
-    var t = '';
-    try { t = this.map[y][x]; }
-    catch(err) { return ut.NULLTILE; }
-    switch(t) {
-    case PacmanGame.PacmanTiles.POINT:
-        return this.TilePoint;
-    case PacmanGame.PacmanTiles.BIGP:
-        return this.TileBigP;
-    case PacmanGame.PacmanTiles.WALL:
-        return this.TileWall;
-    case PacmanGame.PacmanTiles.EMPTY:
-        return this.TileEmpty;
-    default:
-        return ut.NULLTILE;
-    }
-    
+  var t = '';
+  try { t = this.map[y][x]; }
+  catch(err) { return ut.NULLTILE; }
+  switch(t) {
+  case PacmanGame.PacmanTiles.POINT:
+    return this.TilePoint;
+  case PacmanGame.PacmanTiles.BIGP:
+    return this.TileBigP;
+  case PacmanGame.PacmanTiles.WALL:
+    return this.TileWall;
+  case PacmanGame.PacmanTiles.EMPTY:
+    return this.TileEmpty;
+  default:
+    return ut.NULLTILE;
+  }
+  
 };
 
 PacmanGame.prototype.setPacmanTile = function(x, y, tile) {
-    var row = this.map[y];
+  var row = this.map[y];
 
-    this.map[y] = row.substr(0, x) + tile + row.substr(x + 1);
+  this.map[y] = row.substr(0, x) + tile + row.substr(x + 1);
+};
+
+PacmanGame.prototype.setGameOver = function() {
+  this.p1.tile = new ut.Tile('Ⴖ', 255, 255, 255);
+  this.gameOver = true;
+  this.highScore = this.score;
 };
 
 PacmanGame.prototype.putHud = function() {
-    var base = this.termConfig.hud;
+  var base = this.termConfig.hud;
 
-    for (var x = 0; x < this.map[0].length; x++) {
-        for (var y = 0; y < 2; y++) {
-            this.term.put(this.TileHudBG, base.x + x, base.y + y);
-        }
+  for (var x = 0; x < this.map[0].length; x++) {
+    for (var y = 0; y < 2; y++) {
+      this.term.put(this.TileHudBG, base.x + x, base.y + y);
     }
+  }
 
-    var hudbg = PacmanGame.HudColors.BG;
-    var halfLength = this.map[0].length / 2;
+  var hudbg = PacmanGame.HudColors.BG;
+  var halfLength = this.map[0].length / 2;
 
-    this.term.putString("High Score", base.x + halfLength, base.y, 255, 255, 255, hudbg.r, hudbg.g, hudbg.b);
+  this.term.putString("High Score", base.x + halfLength, base.y, 255, 255, 255, hudbg.r, hudbg.g, hudbg.b);
 
-    this.term.putString(this.score.toString(), base.x + 2, base.y + 1, 255, 255, 255, hudbg.r, hudbg.g, hudbg.b);
+  this.term.putString(this.score.toString(), base.x + 2, base.y + 1, 255, 255, 255, hudbg.r, hudbg.g, hudbg.b);
 
-    this.term.putString(this.highScore.toString(), base.x + halfLength, base.y + 1, 255, 255, 255, hudbg.r, hudbg.g, hudbg.b);
+  this.term.putString(this.highScore.toString(), base.x + halfLength, base.y + 1, 255, 255, 255, hudbg.r, hudbg.g, hudbg.b);
 
+  
+  if (this.gameOver) {
+    var mapLength = this.map.length;
+    this.term.putString("Game Over", base.x, base.y + mapLength + 2, 255, 255, 255, hudbg.r, hudbg.g, hudbg.b);
+    this.term.putString("Space to Restart", base.x, base.y + mapLength + 3, 255, 255, 255, hudbg.r, hudbg.g, hudbg.b);
+  }
 };
 
 PacmanGame.prototype.putTiles = function() {
-    var base = this.termConfig.gameArea;
+  var base = this.termConfig.gameArea;
 
-    for (var y = 0; y < this.map.length; y++) {
-        for (var x = 0; x< this.map[0].length; x++) {
-            this.term.put(this.getPacmanTile(x, y), base.x + x, base.y + y);
-        }
+  for (var y = 0; y < this.map.length; y++) {
+    for (var x = 0; x< this.map[0].length; x++) {
+      this.term.put(this.getPacmanTile(x, y), base.x + x, base.y + y);
     }
+  }
 };
 
 PacmanGame.prototype.putMovables = function() {
-    var base = this.termConfig.gameArea;
+  var base = this.termConfig.gameArea;
 
-    this.term.put(this.p1.tile, base.x + this.p1.x, base.y + this.p1.y);
+  this.term.put(this.p1.tile, base.x + this.p1.x, base.y + this.p1.y);
 
-    for (var i in this.ghosts) {
-        var ghost = this.ghosts[i];
+  for (var i in this.ghosts) {
+    var ghost = this.ghosts[i];
 
-        this.term.put(ghost.tile, base.x + ghost.x, base.y + ghost.y);
-    }
+    this.term.put(ghost.tile, base.x + ghost.x, base.y + ghost.y);
+  }
 };
 
 PacmanGame.prototype.movePacman = function(p1V) {
 
-    var p1 = { x: this.p1.x + p1V.x, y: this.p1.y + p1V.y,
-               tile: this.TilePacmanDown
-             };
+  var p1 = { x: this.p1.x + p1V.x, y: this.p1.y + p1V.y,
+             tile: this.TilePacmanDown
+           };
 
-    if (!this.canMoveTo(p1.x, p1.y)) {
-        var wrapPos = this.findWrapPos(p1.x, p1.y);
+  if (this.gameOver) {
+    return;
+  }
 
-        if (wrapPos) {
-            p1.x = wrapPos.x;
-            p1.y = wrapPos.y;
-        } else {
-            return;
-        }
-    }
+  if (!this.canMoveTo(p1.x, p1.y)) {
+    var wrapPos = this.findWrapPos(p1.x, p1.y);
 
-    if (p1V.x < 0) {
-        p1.tile = this.TilePacmanLeft;
-    } else if (p1V.x > 0) {
-        p1.tile = this.TilePacmanRight;
-    } else if (p1V.y < 0) {
-        p1.tile = this.TilePacmanUp;
-    }
-
-    if (p1.tile[0] === this.p1.tile) {
-        p1.tile = p1.tile[1];
+    if (wrapPos) {
+      p1.x = wrapPos.x;
+      p1.y = wrapPos.y;
     } else {
-        p1.tile = p1.tile[0];
+      return;
     }
+  }
 
-    this.p1 = p1;
+  if (p1V.x < 0) {
+    p1.tile = this.TilePacmanLeft;
+  } else if (p1V.x > 0) {
+    p1.tile = this.TilePacmanRight;
+  } else if (p1V.y < 0) {
+    p1.tile = this.TilePacmanUp;
+  }
+
+  if (p1.tile[0] === this.p1.tile) {
+    p1.tile = p1.tile[1];
+  } else {
+    p1.tile = p1.tile[0];
+  }
+
+  this.p1 = p1;
 };
 
 PacmanGame.prototype.spawnGhost = function(color) {
-    var ghost = {
-        x: 14, y: 10,
-        tile: this.getGhostTile(color),
-        v: { x: 0, y: -1 }
-    };
-    
-    this.ghosts.push(ghost);
+  var ghost = {
+    x: 14, y: 10,
+    tile: this.getGhostTile(color),
+    v: { x: 0, y: -1 }
+  };
+  
+  this.ghosts.push(ghost);
 };
 
 PacmanGame.prototype.moveGhost = function(ghost) {
-    var wrapPos = this.findWrapPos(ghost.x + ghost.v.x, ghost.y + ghost.v.y);
+  var wrapPos = this.findWrapPos(ghost.x + ghost.v.x, ghost.y + ghost.v.y);
 
-    if (wrapPos) {
-        ghost.x = wrapPos.x;
-        ghost.y = wrapPos.y;
-        return;
-    }
-    
-    var nextVs = this.availableDirections(ghost.x, ghost.y);
+  if (wrapPos) {
+    ghost.x = wrapPos.x;
+    ghost.y = wrapPos.y;
+    return;
+  }
+  
+  var nextVs = this.availableDirections(ghost.x, ghost.y);
 
-    // protect against going backwards
-    var backwardsV = { x: ghost.v.x * -1, y: ghost.v.y * -1};
-    if (nextVs.length > 1) {
-        nextVs = nextVs
-            .filter(function(v) {
-                return v.x  !==  backwardsV.x  || v.y !== backwardsV.y;
-            });
-    }
+  // protect against going backwards
+  var backwardsV = { x: ghost.v.x * -1, y: ghost.v.y * -1};
+  if (nextVs.length > 1) {
+    nextVs = nextVs
+      .filter(function(v) {
+        return v.x  !==  backwardsV.x  || v.y !== backwardsV.y;
+      });
+  }
 
-    var nextV = Math.floor(Math.random() * (nextVs.length - 1));
-    
-    ghost.v = nextVs[nextV];
+  var nextV = Math.floor(Math.random() * (nextVs.length - 1));
+  
+  ghost.v = nextVs[nextV];
 
-    ghost.x += ghost.v.x;
-    ghost.y += ghost.v.y;
+  ghost.x += ghost.v.x;
+  ghost.y += ghost.v.y;
 };
 
 PacmanGame.prototype.availableDirections = function(x, y) {
-    var res = [];
+  var res = [];
 
-    [{x: 0, y: 1}, {x : 0, y: -1}, { x: 1, y: 0 }, {x: -1, y: 0}]
-        .forEach(function(v) {
-            if (this.canMoveTo(x + v.x, y + v.y)) {
-                res.push(v);
-            }
-        }, this);
+  [{x: 0, y: 1}, {x : 0, y: -1}, { x: 1, y: 0 }, {x: -1, y: 0}]
+    .forEach(function(v) {
+      if (this.canMoveTo(x + v.x, y + v.y)) {
+        res.push(v);
+      }
+    }, this);
 
-    return res;
+  return res;
 };
 
 PacmanGame.prototype.canMoveTo = function(x, y) {
-    var tile = this.getPacmanTile(x, y);
+  var tile = this.getPacmanTile(x, y);
 
-    return (tile === this.TilePoint ||
-            tile === this.TileBigP ||
-            tile === this.TileEmpty);
+  return (tile === this.TilePoint ||
+          tile === this.TileBigP ||
+          tile === this.TileEmpty);
 };
 
 PacmanGame.prototype.findWrapPos = function(x, y) {
-    var wrapPos = this.wrapPos
-            .filter(function(p) {
-                return p.x === x && p.y === y;
-            });
+  var wrapPos = this.wrapPos
+        .filter(function(p) {
+          return p.x === x && p.y === y;
+        });
 
-    if (wrapPos.length > 0) {
-        return wrapPos[0].wrapTo;
-    } else {
-        return null;
-    }
+  if (wrapPos.length > 0) {
+    return wrapPos[0].wrapTo;
+  } else {
+    return null;
+  }
 };
 
 PacmanGame.prototype.checkPacmanEat = function() {
-    var p1Tile = this.getPacmanTile(this.p1.x, this.p1.y);
+  var p1Tile = this.getPacmanTile(this.p1.x, this.p1.y);
 
-    if (p1Tile === this.TilePoint) {
-        this.setPacmanTile(this.p1.x, this.p1.y, PacmanGame.PacmanTiles.EMPTY);
-        this.score++;
+  if (p1Tile === this.TilePoint) {
+    this.setPacmanTile(this.p1.x, this.p1.y, PacmanGame.PacmanTiles.EMPTY);
+    this.score++;
 
-    } else if (p1Tile === this.TileBigP) {
-        this.setPacmanTile(this.p1.x, this.p1.y, PacmanGame.PacmanTiles.EMPTY);
-        this.score += 10;
-    }
+  } else if (p1Tile === this.TileBigP) {
+    this.setPacmanTile(this.p1.x, this.p1.y, PacmanGame.PacmanTiles.EMPTY);
+    this.score += 10;
+  }
 };
 
 PacmanGame.prototype.checkPacmanCaught = function() {
-    
+  var game = this;
+
+  var p1 = this.p1;
+  
+  this.ghosts.forEach(function(g) {
+    if (g.x === p1.x && g.y === p1.y) {
+      game.setGameOver();
+    }
+  });
 };
 
 PacmanGame.prototype.updateGhosts = function() {
-    this.ghosts.forEach(function(g) { this.moveGhost(g); }, this);
+  this.ghosts.forEach(function(g) { this.moveGhost(g); }, this);
 };
 
 PacmanGame.prototype.tick = function() {
+
+  if (!this.gameOver) {
     this.checkPacmanEat();
-    
-    this.putHud();
-    this.putTiles();
-    this.putMovables();
-    this.term.render();
+    this.checkPacmanCaught();
+  }
+  
+  this.putHud();
+  this.putTiles();
+  this.putMovables();
+
+  this.term.render();
 };
 
 PacmanGame.prototype.onKeyDown = function(k) {
-    var moveV = { x: 0, y: 0 };
-    switch (k) {
-    case ut.KEY_W:
-        moveV.y--;
-        break;
-    case ut.KEY_A:
-        moveV.x--;
-        break;
-    case ut.KEY_S:
-        moveV.y++;
-        break;
-    case ut.KEY_D:
-        moveV.x++;
-        break;
-    }
+  var moveV = { x: 0, y: 0 };
+  switch (k) {
+  case ut.KEY_W:
+    moveV.y--;
+    break;
+  case ut.KEY_A:
+    moveV.x--;
+    break;
+  case ut.KEY_S:
+    moveV.y++;
+    break;
+  case ut.KEY_D:
+    moveV.x++;
+    break;
+  }
 
-    if (typeof this.onPacmanMove === 'function') {
-        this.onPacmanMove(moveV);
-    }
+  if (typeof this.onPacmanMove === 'function') {
+    this.onPacmanMove(moveV);
+  }
 };
 
 
 PacmanGame.prototype.start = function() {
-    this.generateMap();
-    
-    this.term = new ut.Viewport(this.parent, 50, 30);
-    ut.initInput(this.onKeyDown.bind(this));
+  this.gameOver = false;
 
-    this.tick();
+  this.generateMap();
+  
+  this.term = new ut.Viewport(this.parent, 50, 30);
+  ut.initInput(this.onKeyDown.bind(this));
+
+  this.tick();
 };
